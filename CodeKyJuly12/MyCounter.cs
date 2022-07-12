508be35deb7fe997884b8340570886c65af600fe
Jared Hatfield <jared.hatfield@geappliances.com>
2022-07-12 19:53:03 -0400

    Demo C# project to show class structure

---
+++ CodeKyJuly12/MyCounter.cs
@@ -0,0 +1,37 @@
+﻿using System;
+namespace CodeKyJuly12
+{
+	public class MyCounter
+	{
+		private int _happy;
+
+		private int _sad;
+	
+		public MyCounter()
+		{
+			_happy = 10;
+			_sad = 0;
+		}
+
+		public void happyCustomer()
+        {
+			this._happy++;
+        }
+
+		public void sadCustomer()
+        {
+			this._sad++;
+        }
+
+		public int allCustomers()
+        {
+			return _happy + _sad;
+        }
+
+		public void saveToFile()
+        {
+			// TODO: save this to file
+        }
+	}
+}
+
---
+++ CodeKyJuly12/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CodeKyJuly12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            MyCounter myCounter = new MyCounter();
+
+            myCounter.happyCustomer();
+            myCounter.sadCustomer();
+            myCounter.happyCustomer();
+
+
+            MyCounter other = new MyCounter();
+
+
+            Console.WriteLine("Total: " + myCounter.allCustomers());
+        }
+    }
+}
+
